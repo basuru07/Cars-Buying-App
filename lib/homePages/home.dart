e1f381a78d9ba6ddcddd868e5fb086e7731922c5
Basuru Yasaruwan <yasaruwanbasuru637@gmail.com>
2025-02-25 17:23:03 +0530

    home page

--- lib/homePages/home.dart
+++ lib/homePages/home.dart
@@ -19,6 +19,7 @@ class _HomePageState extends State<HomePage> {
     return Scaffold(
       key: _scaffoldKey, // Add the key to Scaffold
       appBar: AppBar(
+        backgroundColor: Colors.white,
         leading: IconButton(
           icon: Image.asset(
               "assets/heroicons-outline_menu-alt-2.png"), // Menu Image
@@ -56,16 +57,72 @@ class _HomePageState extends State<HomePage> {
         ],
       ),
       drawer: DrawerMenu(), // Use the separate DrawerMenu widget
-      body: Column(
-        children: [
-          SizedBox(
-            height: 20,
+      body: Container(
+        color: Colors.white,
+        child: SingleChildScrollView(
+          child: Column(
+            children: [
+              SizedBox(
+                height: 20,
+              ),
+              homenamebar(),
+              BrandNameRow(),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                children: [
+                  Image.asset("assets/Group 79.png"),
+                  Image.asset("assets/Group 80.png"),
+                  Image.asset("assets/Group 81.png")
+                ],
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                children: [
+                  Image.asset("assets/Group 79 (1).png"),
+                  Image.asset("assets/Group 80 (1).png"),
+                  Image.asset("assets/Group 81 (1).png")
+                ],
+              ),
+              SizedBox(
+                height: 25,
+              ),
+              Padding(
+                padding: const EdgeInsets.all(15.0),
+                child: Container(
+                  height: 200,
+                  width: 400,
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(20),
+                    color: Color(0xFFF6F8F8),
+                    image: DecorationImage(
+                      image: AssetImage("assets/Group 129.png"),
+                      fit: BoxFit.fill
+                          , // Keeps the image within the container
+                    ),
+                  ),
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.all(15.0),
+                child: Container(
+                  height: 200,
+                  width: 400,
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(20),
+                    color: Color(0xFFF6F8F8),
+                    image: DecorationImage(
+                      image: AssetImage("assets/Group 128.png"),
+                      fit: BoxFit.fill
+                          , // Keeps the image within the container
+                    ),
+                  ),
+                ),
+              ),
+              
+            ],
           ),
-          homenamebar(),
-          BrandNameRow(),
-        ],
+        ),
       ),
     );
   }
 }
-
